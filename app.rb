b75aba63a9a798e82e150effca6b9954adf957b7
Stuart Black <stuartblack@gmail.com>
2018-08-03 11:09:14 +0100

    Vicky

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/patron_controller')
 require_relative('controllers/review_controller')
 require_relative('controllers/recipe_controller')
